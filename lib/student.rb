b26123270e22948cfe97f3a9ce87b741d0ba6822
Oana Berentan <oana.berentan@yahoo.com>
2019-09-22 15:09:27 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,7 +85,7 @@ class Student
     WHERE grade = 10
     LIMIT x
     SQL
-    DB[:conn].execute(sql, students.grade).map do |row|
+    DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
     end
 
