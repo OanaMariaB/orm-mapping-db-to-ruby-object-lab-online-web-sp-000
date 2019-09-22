bb2b0e4fe75cfabca01c513c4606a8a1fa134fb5
Oana Berentan <oana.berentan@yahoo.com>
2019-09-22 14:25:02 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -63,6 +63,8 @@ class Student
   end
 
   def self.all_students_in_grade_9
-    sql = "SELECT COUNT(*) FROM students WHERE grade=9;"
+    sql = <<-SQL 
+    SELECT COUNT(*) FROM students WHERE grade = 9 
+    SQL
     DB[:conn].execute(sql)
   end
