1ab2c450366d8ad893573de3a9c54963b751297f
Oana Berentan <oana.berentan@yahoo.com>
2019-09-22 15:25:24 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -95,12 +95,11 @@ class Student
     SELECT *
     FROM students
     WHERE grade = 10
-    ORDER BY id 
     LIMIT 1
     SQL
     DB[:conn].execute(sql).map do |row|
       self.new_from_db(row)
-    end
+    end.first
   end
 
 end
